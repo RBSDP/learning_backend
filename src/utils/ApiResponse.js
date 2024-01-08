d4d5dee2f26512169174c0b6e055030585e168cf
RAAVI BALA SIVA DURGA PRASAD <84697652+RBSDP@users.noreply.github.com>
2024-01-08 11:25:17 +0000

    added apiResponce and ApiError in utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(starusCode,messaage="Success",data){
+        this.statusCode =statusCode
+        this.data = data
+        this.message = this.message
+        this.success = starusCode < 400
+
+    }
+}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,21 @@
-const asyncHandler = () => {
+// const asyncHandler = (hadlerFunction) => async(req,res,next) =>{
 
+//     try {
+//         await hadleerFunction(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+
+// }
+
+// using promises
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
 }
 
 export {asyncHandler}
